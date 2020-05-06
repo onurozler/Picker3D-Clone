b0a4594dc7d6d44dc49977ffeb4dd9a5ced8a7e6
onurozler <onur.ozler@ug.bilkent.edu.tr>
2020-05-06 21:11:55 +0300

    Check point UI added

---
+++ Assets/Scripts/Game/View/CheckPointProgression.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Game.GameEvents;
+using Game.View.Helpers;
+using UnityEngine;
+
+namespace Game.View
+{
+    public class CheckPointProgression : MonoBehaviour
+    {
+        private List<CheckItem> _checkItems;
+
+        private void Awake()
+        {
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            _checkItems = GetComponentsInChildren<CheckItem>().ToList();
+            foreach (var checkItem in _checkItems)
+            {
+                checkItem.Initialize();
+            }
+            
+            GameEventBus.SubscribeEvent(GameEventType.CHECKPOINT, ActiveCheckPointItem);
+            GameEventBus.SubscribeEvent(GameEventType.FINISHED, DeActivateCheckPointItem);
+        }
+
+        private void DeActivateCheckPointItem()
+        {
+            foreach (var item in _checkItems)
+            {
+                item.Deactive();
+            }
+        }
+
+        private void ActiveCheckPointItem()
+        {
+            foreach (var item in _checkItems)
+            {
+                if (!item.IsActive)
+                {
+                    item.Active();
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Game/View/Helpers/CheckItem.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.View.Helpers
+{
+    public class CheckItem : MonoBehaviour
+    {
+        private Image _checkImage;
+
+        public bool IsActive;
+        
+        public void Initialize()
+        {
+            _checkImage = GetComponent<Image>();
+            IsActive = false;
+        }
+
+        public void Active()
+        {
+            IsActive = true;
+            _checkImage.color = Color.yellow;
+        }
+
+        public void Deactive()
+        {
+            IsActive = false;
+            _checkImage.color = Color.white;
+        }
+    }
+}
